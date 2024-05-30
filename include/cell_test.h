ad90d59676a759cb3552d3b8da8e20ce365f5a4f
Sadidev <67427887+Sadidev@users.noreply.github.com>
2024-05-30 17:01:53 -0300

    Added cell test for calibration

--- include/cell_test.h
+++ include/cell_test.h
@@ -2,6 +2,8 @@
 #define CELL_TEST_H
 #include "HX711.h"
 
+void calibrateCell(HX711 scale);
+
 void testCell(HX711 scale);
 
 #endif // CELL_TEST_H
--- src/cell_test.cpp
+++ src/cell_test.cpp
@@ -3,7 +3,7 @@
 #include "pins.h"
 #include "HX711.h"
 
-void testCell(HX711 scale) {
+void calibrateCell(HX711 scale) {
   if (scale.is_ready()) {
     scale.set_scale();    
     Serial.println("Tare... remove any weights from the scale.");
@@ -20,4 +20,15 @@ void testCell(HX711 scale) {
     Serial.println("HX711 not found.");
   }
   delay(1000);
+}
+
+void testCell(HX711 scale) {
+  Serial.print("one reading:\t");
+  Serial.print(scale.get_units(), 1);
+  Serial.print("\t| average:\t");
+  Serial.println(scale.get_units(10), 5);
+
+  scale.power_down(); // put the ADC in sleep mode
+  delay(5000);
+  scale.power_up();
 }
