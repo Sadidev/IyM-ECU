f0bcaf0502aee263cb8a746e048c0b8f420011e3
Sadidev <67427887+Sadidev@users.noreply.github.com>
2024-06-05 17:56:56 -0300

    Added pot value check

--- src/accelerator.cpp
+++ src/accelerator.cpp
@@ -5,6 +5,9 @@
 float getAcceleratorValue8Bits() {
     float initialValue = analogRead(ACCELERATOR_PIN);
     float convertedValue = initialValue * (255.0/4095.0);
+    
+    Serial.print("Pot: ");
+    Serial.println(convertedValue);
 
     return convertedValue;
 }
