7b874930bea482877e4ce135b17be33c2c0445c5
Sadidev <67427887+Sadidev@users.noreply.github.com>
2024-06-10 21:25:51 -0300

    Defined missing pins

--- include/pins.h
+++ include/pins.h
@@ -1,14 +1,14 @@
 #ifndef PINS_H
 #define PINS_H
 
-// Define pins used below here
+// Pins utilizados
 #define ACCELERATOR_PIN 34
 #define LOADCELL_DOUT_PIN 3
 #define LOADCELL_SCK_PIN 22
 #define MOTOR_PIN 13
 #define SEATBELT_PIN 27
 #define ENCODER_PIN 35
-#define BREAK_PIN 5
+#define BREAK_PIN 14
 #define TEMPERATURE_PIN 32
 
 #endif // PINS_H
